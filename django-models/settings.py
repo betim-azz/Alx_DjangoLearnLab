9e86b0112f768b71c50a423f23693ba07c6682f9
bethelehem Melaku <betim7995@gmail.com>
2026-03-07 23:39:27 -0800

    Implement advanced model relationships with ForeignKey, ManyToMany, and OneToOne

--- django-models/settings.py
+++ django-models/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
