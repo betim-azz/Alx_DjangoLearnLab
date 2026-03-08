e26a4bafe22a2ab015fda3251308d3b0491c0f65
bethelehem Melaku <betim7995@gmail.com>
2026-03-08 00:17:54 -0800

    Fix ExampleForm import in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import BookForm
+from .forms import ExampleForm
 
 # Task 1: Permission-protected views
 @permission_required('bookshelf.can_view', raise_exception=True)
