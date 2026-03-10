92c227ee651ac750a2863bb1fdcded71763ac1bc
bethelehem Melaku <betim7995@gmail.com>
2026-03-10 04:36:58 -0700

    Add exact strings required by checker in serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,21 +11,22 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ['followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
+            password=validated_data['password']
         )
+        if 'bio' in validated_data:
+            user.bio = validated_data['bio']
         if 'profile_picture' in validated_data:
             user.profile_picture = validated_data['profile_picture']
-            user.save()
+        user.save()
         Token.objects.create(user=user)
         return user
